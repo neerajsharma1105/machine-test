7f4a876bdf97bf05e106edc6df67c14aa926ac7e
neerajsharma1105 <87375195+neerajsharma1105@users.noreply.github.com>
2024-07-26 23:54:32 +0530

    Update Add.js

--- src/component/Pages/Crud/Add.js
+++ src/component/Pages/Crud/Add.js
@@ -16,13 +16,23 @@ import { Form, Formik } from "formik";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { addEmployee } from "../../../store/employeeSlice";
+import * as Yup from 'yup';
 
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  gender: Yup.string().required('Gender is required'),
+  status: Yup.string().required('Status is required'),
+  email: Yup.string().email('Invalid email format').required('Email is required'),
+});
 const Add = () => {
+
   const dispatch = useDispatch();
 
   return (
     <div>
       <Formik
+        enableReinitialize
+        validationSchema={validationSchema}
         initialValues={{ name: "", email: "", gender: "", status: "" }}
         onSubmit={(values, { setSubmitting, resetForm }) => {
           dispatch(addEmployee(values));
@@ -30,7 +40,7 @@ const Add = () => {
           resetForm();
         }}
       >
-        {({ values, handleChange, handleSubmit, isSubmitting }) => (
+        {({ values, handleChange, handleSubmit, isSubmitting, errors, touched }) => (
           <Form onSubmit={handleSubmit}>
             <Box
               sx={{
@@ -51,6 +61,8 @@ const Add = () => {
                     value={values.name}
                     onChange={handleChange}
                     margin="normal"
+                    error={touched.name && Boolean(errors.name)}
+                    helperText={touched.name && errors.name}
                   />
                 </Grid>
                 <Grid item xs={12} sm={12}>
@@ -62,10 +74,12 @@ const Add = () => {
                     value={values.email}
                     onChange={handleChange}
                     margin="normal"
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
                   />
                 </Grid>
                 <Grid item xs={12} sm={12}>
-                  <FormControl component="fieldset" margin="normal">
+                  <FormControl component="fieldset" margin="normal" error={touched.gender && Boolean(errors.gender)}>
                     <FormLabel component="legend">Gender</FormLabel>
                     <RadioGroup
                       aria-labelledby="gender"
@@ -85,10 +99,12 @@ const Add = () => {
                         label="Male"
                       />
                     </RadioGroup>
+                    {touched.gender && errors.gender && <div style={{ color: 'red' }}>{errors.gender}</div>}
+
                   </FormControl>
                 </Grid>
                 <Grid item xs={12} sm={12}>
-                  <FormControl margin="normal" sx={{ width: 230 }}>
+                  <FormControl margin="normal" sx={{ width: 230 }} error={touched.status && Boolean(errors.status)}>
                     <InputLabel id="status">Status</InputLabel>
                     <Select
                       size="small"
@@ -102,6 +118,8 @@ const Add = () => {
                       <MenuItem value="Active">Active</MenuItem>
                       <MenuItem value="InActive">InActive</MenuItem>
                     </Select>
+                    {touched.status && errors.status && <div style={{ color: 'red' }}>{errors.status}</div>}
+
                   </FormControl>
                 </Grid>
               </Grid>
